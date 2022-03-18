e6630f9b5248aa40eddd395d44d2527c12b8d960
user <user@DESKTOP-5SRAAK8>
2022-03-18 21:41:18 +0000

    day,review,session,and timetable models added

---
+++ src/Day.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author user
+ */
+public class Day {
+    private int ID;
+    private Session[] Sessions;
+    
+    private Day(int id,Session[] sessions)
+    {
+        ID=id;
+        Sessions=sessions;
+    }
+    
+}
